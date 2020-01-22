de5085fd6f0f2b90ff0b263564d5f8fd0489d05a
dbeauchaine <danbeauchaine@gmail.com>
2020-01-22 14:55:53 -0800

    updated tests in tables with proper fake data

--- src/app/basic-batting-table/basic-batting-table.component.spec.ts
+++ src/app/basic-batting-table/basic-batting-table.component.spec.ts
@@ -1,6 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BasicBattingTableComponent } from './basic-batting-table.component';
+import { Batting } from '../batting';
+import { MatCardModule, MatFormFieldModule, MatTableModule, MatTooltipModule, MatPaginatorModule, MatSortModule, MatInputModule } from '@angular/material';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { By } from '@angular/platform-browser';
 
 describe('BasicBattingTableComponent', () => {
   let component: BasicBattingTableComponent;
@@ -8,7 +12,19 @@ describe('BasicBattingTableComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ BasicBattingTableComponent ]
+      imports: [
+        MatCardModule,
+        MatFormFieldModule,
+        MatTableModule,
+        MatTooltipModule,
+        MatPaginatorModule,
+        MatSortModule,
+        MatInputModule,
+        NoopAnimationsModule
+      ],
+      declarations: [ 
+        BasicBattingTableComponent 
+      ]
     })
     .compileComponents();
   }));
@@ -16,10 +32,45 @@ describe('BasicBattingTableComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(BasicBattingTableComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    component.data = createFakeBatting();
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    fixture.detectChanges();
+    const headers = fixture.debugElement.queryAll(By.css('mat-header-cell'));
+    headers.forEach(header =>{
+      expect(component.displayedColumns.includes(header.componentInstance));
+    });
   });
+
+  function createFakeBatting(): Batting[] {
+    const batting = new Batting();
+    batting.playerId = 'expectedPlayerId';
+    batting.yearId = 1999;
+    batting.stint = 1;
+    batting.teamId = 'SEA';
+    batting.lgId = 'AL';
+    batting.g = 100;
+    batting.gBatting = 99;
+    batting.ab = 99;
+    batting.r = 50;
+    batting.h = 20;
+    batting.x2b = 10;
+    batting.x3b = 3;
+    batting.hr = 20;
+    batting.rbi = 40;
+    batting.sb = 10;
+    batting.cs = 5;
+    batting.bb = 20;
+    batting.so = 25;
+    batting.ibb = 1;
+    batting.hbp = 1;
+    batting.sh = 3;
+    batting.sf = 2;
+    batting.gidp = 1;
+
+    const fakeBattings: Array<Batting> = [batting];
+
+    return fakeBattings;
+}
 });
--- src/app/basic-pitching-table/basic-pitching-table.component.spec.ts
+++ src/app/basic-pitching-table/basic-pitching-table.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BasicPitchingTableComponent } from './basic-pitching-table.component';
+import { MatCardModule, MatFormFieldModule, MatTableModule, MatTooltipModule, MatPaginatorModule, MatSortModule, MatInputModule } from '@angular/material';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { Pitching } from '../pitching';
 
 describe('BasicPitchingTableComponent', () => {
   let component: BasicPitchingTableComponent;
@@ -8,18 +11,68 @@ describe('BasicPitchingTableComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ BasicPitchingTableComponent ]
+      imports: [
+        MatCardModule,
+        MatFormFieldModule,
+        MatTableModule,
+        MatTooltipModule,
+        MatPaginatorModule,
+        MatSortModule,
+        MatInputModule,
+        NoopAnimationsModule
+      ],
+      declarations: [
+        BasicPitchingTableComponent
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(BasicPitchingTableComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    component.data = createFakePitching();
   });
 
   it('should create', () => {
+    fixture.detectChanges();
     expect(component).toBeTruthy();
   });
+
+  function createFakePitching(): Pitching[] {
+    const pitching = new Pitching()
+    pitching.playerId = 'expectedPlayerId';
+    pitching.yearId = 1999;
+    pitching.stint = 1;
+    pitching.teamId = 'SEA';
+    pitching.w = 2;
+    pitching.l = 1;
+    pitching.g = 10;
+    pitching.gs = 8;
+    pitching.cg = 2;
+    pitching.sho = 1;
+    pitching.sv = 1;
+    pitching.ipouts = 2;
+    pitching.h = 1;
+    pitching.er = 2;
+    pitching.hr = 1;
+    pitching.bb = 5;
+    pitching.so = 3;
+    pitching.baopp = 5;
+    pitching.era = 3.23;
+    pitching.ibb = 2;
+    pitching.wp = 3;
+    pitching.hbp = 6;
+    pitching.bk = 5;
+    pitching.bfp = 1;
+    pitching.gf = 2;
+    pitching.r = 3;
+    pitching.sh = 4;
+    pitching.sf = 5;
+    pitching.gidp = 6;
+
+    const fakePitching: Array<Pitching> = [pitching];
+
+    return fakePitching;
+  }
 });

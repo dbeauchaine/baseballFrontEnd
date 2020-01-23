9bed9e5365bd559e0f86325c85d737a7017a7a6e
dbeauchaine <danbeauchaine@gmail.com>
2020-01-23 10:16:06 -0800

    Began implementation of player-search component tests Fixed Imports.

--- src/app/player-search/player-search.component.spec.ts
+++ src/app/player-search/player-search.component.spec.ts
@@ -1,25 +1,74 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PlayerSearchComponent } from './player-search.component';
+import { MatFormFieldModule, MatAutocompleteModule, MatProgressSpinnerModule } from '@angular/material';
+import { ReactiveFormsModule, FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { PlayerService } from '../player.service';
+import { of } from 'rxjs';
+import { Player } from '../player';
 
 describe('PlayerSearchComponent', () => {
   let component: PlayerSearchComponent;
   let fixture: ComponentFixture<PlayerSearchComponent>;
-
+  const fakePlayer = createFakePlayer();
+  let mockPlayerService: PlayerService;
+  let mockFormBuilder: FormBuilder;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PlayerSearchComponent ]
+      imports: [
+        MatFormFieldModule,
+        ReactiveFormsModule,
+        MatAutocompleteModule,
+        MatProgressSpinnerModule,
+        RouterTestingModule
+      ],
+      declarations: [
+        PlayerSearchComponent
+      ],
+      providers: [
+        {
+          provide: FormBuilder,
+          useValue:
+            {
+              group() {
+                return new FormGroup({id:new FormControl(fakePlayer.playerId)});
+              }
+            },
+        },
+        {
+          provide: PlayerService,
+          useValue:
+              {
+                  getPlayer(id: string) {
+                      expect(id).toEqual(fakePlayer.playerId);
+                      return of(fakePlayer);
+                  }
+              } as PlayerService,
+      },
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(PlayerSearchComponent);
+    mockPlayerService = TestBed.get(PlayerService);
+    mockFormBuilder = TestBed.get(FormBuilder);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
+    fixture.detectChanges();
     expect(component).toBeTruthy();
   });
+
+  function createFakePlayer(): Player {
+    const player = new Player();
+    player.playerId = 'id';
+    player.nameFirst = 'expectedFirstName';
+    player.nameLast = 'expectedLastName';
+
+    return player;
+}
 });

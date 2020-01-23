2014a3061aceaa923329dae563998e28a6a0f80f
dbeauchaine <danbeauchaine@gmail.com>
2020-01-23 10:53:44 -0800

    Tested team service Tested team service get by id and year. Minor naming changes to pitching service tests.

--- src/app/team.service.spec.ts
+++ src/app/team.service.spec.ts
@@ -1,12 +1,79 @@
 import { TestBed } from '@angular/core/testing';
 
 import { TeamService } from './team.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
-describe('TeamService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+fdescribe('TeamService', () => {
+  let httpMock: HttpTestingController;
+  let service: TeamService;
 
-  it('should be created', () => {
-    const service: TeamService = TestBed.get(TeamService);
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+        providers: [TeamService],
+        imports: [HttpClientTestingModule],
+    });
+
+    httpMock = TestBed.get(HttpTestingController);
+    service = TestBed.get(TeamService);
+});
+
+  afterEach(() => {
+    httpMock.verify();
+});
+
+it('should be created', () => {
     expect(service).toBeTruthy();
+});
+
+describe('Getting Team Data', () => {
+    it('gets team data from team service by id', () => {
+        const mockTeam = {
+            teamId: 'id',
+            yearId: 2000,
+            w: 20,
+            hr: 1
+        }
+
+        service.getTeamStats(mockTeam.teamId)
+            .subscribe(data => {
+                expect(data[0].teamId).toEqual(mockTeam.teamId);
+                expect(data[0].w).toEqual(mockTeam.w);
+                expect(data[0].hr).toEqual(mockTeam.hr);
+                expect(data[0].yearId).toEqual(mockTeam.yearId);
+            });
+
+        const req = httpMock.expectOne(
+            `https://localhost:5001/team/${mockTeam.teamId}`
+        );
+
+        expect(req.request.method).toEqual('GET');
+
+        req.flush([mockTeam]);
+    });
+
+    it('gets team stats from team service by year', () => {
+      const mockTeam = {
+          teamId: 'id',
+          yearId: 2000,
+          w: 20,
+          hr: 1
+      }
+
+      service.getTeamStatsByYear(mockTeam.yearId)
+          .subscribe(data => {
+              expect(data[0].teamId).toEqual(mockTeam.teamId);
+              expect(data[0].w).toEqual(mockTeam.w);
+              expect(data[0].hr).toEqual(mockTeam.hr);
+              expect(data[0].yearId).toEqual(mockTeam.yearId);
+          });
+
+      const req = httpMock.expectOne(
+          `https://localhost:5001/team/year/${mockTeam.yearId}`
+      );
+
+      expect(req.request.method).toEqual('GET');
+
+      req.flush([mockTeam]);
   });
+});
 });

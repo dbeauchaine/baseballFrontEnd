f8d2827418904a9b39da5d0f2d836e7221f5c38b
dbeauchaine <danbeauchaine@gmail.com>
2020-01-24 15:25:10 -0800

    Added postseason fielding data to fielding service and added column definitions for postseason fielding and pitching data

--- src/app/columnDefinitions.ts
+++ src/app/columnDefinitions.ts
@@ -94,6 +94,27 @@ export class ColumnDefinitions {
         ]);
     }
 
+    fieldingPostBox(): Map<string,ColumnConfig>{
+        return new Map([
+            ['yearId', { columnName: "Year" }],
+            ['lgId', {columnName: "League", displayFormat: DisplayFormat.Logo}],
+            ['teamId', { columnName: 'Team', displayFormat: DisplayFormat.Logo}],
+            ['round', { columnName: 'Round', headerTooltip: "Playoff Round"}],
+            ['pos', { columnName: 'POS', headerTooltip: "Games" }],
+            ['g', { columnName: 'G', headerTooltip: "Games" }],
+            ['gs', { columnName: 'GS', headerTooltip: "Games Started" }],
+            ['innOuts', { columnName: 'InnOuts', headerTooltip: "Outs Played on Defense" }],
+            ['po', { columnName: 'PO', headerTooltip: "Put Outs" }],
+            ['a', { columnName: 'A', headerTooltip: "Assists" }],
+            ['e', { columnName: 'E', headerTooltip: "Errors" }],
+            ['dp', { columnName: 'DP', headerTooltip: "Double Plays" }],
+            ['tp', { columnName: 'TP', headerTooltip: "Triple Plays" }],
+            ['pb', { columnName: 'PB', headerTooltip: "Passed Balls" }],
+            ['sb', { columnName: 'SB', headerTooltip: "Stolen Bases Against"}],
+            ['cs', { columnName: 'CS', headerTooltip: "Caught Stealing"}],
+        ]);
+    }
+
     pitchingBox(): Map<string,ColumnConfig>{
         return new Map([
             ['yearId', { columnName: "Year" }],
@@ -213,6 +234,41 @@ export class ColumnDefinitions {
             ['babip', { columnName: 'BABIP', headerTooltip: "Batting Average On Balls In Play", numberFormat:NumberFormat.Decimal}],
             ['bbRate', { columnName: 'BBRate', headerTooltip: "Walk Rate", numberFormat:NumberFormat.Decimal}],
             ['kRate', { columnName: 'KRate', headerTooltip: "Strikeout Rate", numberFormat:NumberFormat.Decimal}],
+        ]);
+    }
+
+    
+    pitchingLeaderboard(): Map<string,ColumnConfig>{
+        return new Map([
+            ['nameFirst', { columnName: "First Name" }],
+            ['nameLast', { columnName: "Last Name" }],
+            ['yearId', { columnName: "Year" }],
+            ['lgId', {columnName: "League", displayFormat: DisplayFormat.Logo}],
+            ['teamId', { columnName: 'Team', displayFormat: DisplayFormat.Logo}],
+            ['w', { columnName: 'W', headerTooltip: "Wins" }],
+            ['l', { columnName: 'L', headerTooltip: "Losses" }],
+            ['g', { columnName: 'G', headerTooltip: "Games" }],
+            ['gs', { columnName: 'GS', headerTooltip: "Games Started" }],
+            ['cg', { columnName: 'CG', headerTooltip: "Complete Games" }],
+            ['sho', { columnName: 'SHO', headerTooltip: "Shutouts" }],
+            ['sv', { columnName: 'SV', headerTooltip: "Saves" }],
+            ['ipouts', { columnName: 'IpOuts', headerTooltip: "Outs Pitched" }],
+            ['h', { columnName: 'H', headerTooltip: "Hits" }],
+            ['er', { columnName: 'ER', headerTooltip: "Earned Runs Allowed"}],
+            ['hr', { columnName: 'HR', headerTooltip: "Homeruns"}],
+            ['bb', { columnName: 'BB', headerTooltip: "Walks"}],
+            ['so', { columnName: 'K', headerTooltip: "Strikeouts"}],
+            ['baopp', { columnName: 'BAOpp', headerTooltip: "Opponents' Batting Average"}],
+            ['era', { columnName: 'ERA', headerTooltip: "Earned Run Average", numberFormat:NumberFormat.Decimal}],
+            ['ibb', { columnName: 'IBB', headerTooltip: "Intentional Walks"}],
+            ['wp', { columnName: 'WP', headerTooltip: "Wild Pitches"}],
+            ['bk', { columnName: 'BK', headerTooltip: "Balks"}],
+            ['bfp', { columnName: 'BFP', headerTooltip: "Batters Faced By Pitcher"}],
+            ['gf', { columnName: 'GF', headerTooltip: "Games Finished"}],
+            ['r', { columnName: 'R', headerTooltip: "Runs Allowed"}],
+            ['sh', { columnName: 'SH', headerTooltip: "Sacrifice Hits"}],
+            ['sf', { columnName: 'SF', headerTooltip: "Sacrifice Flies"}],
+            ['gidp', { columnName: 'GIDP', headerTooltip: "Ground Into Double Play Induced"}],
         ]);
     }
 
